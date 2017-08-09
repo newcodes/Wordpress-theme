d68322896dc215b1362816c3707e7e1c20637ef0
Serhii Tytar <serhii.tytar@gmail.com>
2017-08-09 17:27:35 +0300

    add page&contact

---
+++ wp-content/themes/kitchen/page.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Page
+*/ 
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				  <?php if (have_posts()) : ?>
+					<?php while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+					<?php endwhile; ?>
+					<?php endif; ?>
+		</div>
+	</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
