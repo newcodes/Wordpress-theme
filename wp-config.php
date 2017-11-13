ee49c99f3d39b37ea87cff633135e576a3066335
Serhii Tytar <serhii.tytar@gmail.com>
2017-11-13 13:15:37 +0200

    change config, delete assets

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'mdvd_armadio');
+define('DB_NAME', 'mdvd_armadio1');
 
 /** MySQL database username */
 define('DB_USER', 'root');
